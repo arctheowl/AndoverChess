81f97c91cc83e3ea73527e763034cd5787c38683
lidevlin <lidevlin95@gmail.com>
2023-10-08 23:23:38 +0100

    Fixed api key

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,7 +3,7 @@ export default function Map() {
     <iframe
       loading="lazy"
       className="rounded-xl w-full h-full "
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB-71eJpa_yXb09ZthDk0zJpD9HMdJzaZE&q=Andover+Central+Club"
+      src={`https://www.google.com/maps/embed/v1/place?key=${process.env.GAPI_KEY}&q=Andover+Central+Club`}
     />
   );
 }
