f24389a9a2aa2db202bd55690f8e77cc165774dc
lidevlin <lidevlin95@gmail.com>
2023-10-09 21:44:39 +0100

    Added proper handling for routes on mobile menu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,9 +1,17 @@
 "use client";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+
+import { Disclosure } from "@headlessui/react";
+import { useSearchParams, useParams, usePathname } from "next/navigation";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross1 } from "react-icons/rx";
 
-export default function Example() {
+interface IMobileMenu {
+  page: string;
+}
+
+export default function MobileMenu() {
+  const pathname = usePathname();
+
   return (
     <div className="flex md:hidden">
       <Disclosure as="nav" className="top-2 bg-slate-100">
@@ -25,28 +33,44 @@ export default function Example() {
                 <Disclosure.Button
                   as="a"
                   href="/about"
-                  className="border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700  w-64"
+                  className={`${
+                    pathname === "/about"
+                      ? "border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+                      : "block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  }`}
                 >
                   About
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="/fixtures"
-                  className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  className={`${
+                    pathname === "/fixtures"
+                      ? "border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+                      : "block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  }`}
                 >
                   Fixtures
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="/articles"
-                  className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  className={`${
+                    pathname === "/articles"
+                      ? "border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+                      : "block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  }`}
                 >
                   Articles
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="/tools"
-                  className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  className={`${
+                    pathname === "/tools"
+                      ? "border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+                      : "block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+                  }`}
                 >
                   Tools
                 </Disclosure.Button>
