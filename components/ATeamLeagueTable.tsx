c9c1fd967efc4744c062f8cf79ee865d64e6211d
lidevlin <lidevlin95@gmail.com>
2024-01-02 15:33:10 +0000

    Testing

--- components/ATeamLeagueTable.tsx
+++ components/ATeamLeagueTable.tsx
@@ -91,7 +91,7 @@ const LeagueTable = async () => {
                     <div className="text-gray-900">{teamIdx + 1}</div>
                   </td>
                   <td className="border-r border-gray-600">{team?.team}</td>
-                  <td className="border-r border-gray-600">{team?.played}</td>
+                  {/* <td className="border-r border-gray-600">{team?.played}</td>
                   <td className="border-r border-gray-600">{team?.wins}</td>
                   <td className="border-r border-gray-600">
                     <div className="">{team?.draws}</div>
@@ -110,7 +110,7 @@ const LeagueTable = async () => {
                   </td>
                   <td className="border-r border-gray-600">
                     <div className="">{team?.points}</div>
-                  </td>
+                  </td> */}
                   {/* <td className="hidden md:table-cell">
                     <div className="flex gap-3 pl-2">
                       {Form.map((id, result) => {
